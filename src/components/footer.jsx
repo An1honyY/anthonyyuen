3159bf6606325cbf5f9a496df17980a3c520ea73
An1honyY <ant.jo.yuen@gmail.com>
2024-03-05 14:49:04 +1300

    Added Footer Icons and Styling
    Also changed header shade and gradient

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,15 +1,41 @@
 import React from "react";
 import "./footer.css";
+import linkedin from '../assets/linkedin-icon.svg'
+import github from '../assets/github-icon.svg'
+import email from '../assets/email-icon.svg'
 
 function Footer() {
+    function copyToClipboard() {
+        const email = "ant.jo.yuen@gmail.com";
+        navigator.clipboard.writeText(email).then(() => {
+            alert("Email copied to clipboard");
+        });
+    }
+
     return (
-        <>
-            <footer>
+        <footer>
+            <div className="triangle-border"></div>
+            <div className="polygon-fill">
+                <div className="content">
+                    Thanks for Visiting!
+                    <br></br>
+                    <br></br>
+
+                    Check out my other projects on GitHub
+                    <br></br>
+                    Or contact me via Email or LinkedIn
+                </div>
+                <div className="icons">
+                    <a href="https://linkedin.com/in/anthony-yuen-089208229"><img src={linkedin} alt="linkednin-icon" /></a>
+                    <a href="https://github.com/An1honyY"><img src={github} alt="github-icon" /></a>
+                    <img src={email} alt="email-icon" onClick={copyToClipboard} />
+                </div>
+
                 <p className="copyright">
                     © 2024 Anthony Yuen. All rights reserved.
                 </p>
-            </footer>
-        </>
+            </div>
+        </footer>
     )
 }
 
