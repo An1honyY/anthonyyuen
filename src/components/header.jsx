1024eb6c5fafa1657b327e45f2c53273190d0774
An1honyY <ant.jo.yuen@gmail.com>
2024-03-05 21:08:55 +1300

    Added Burger Menu and edit CV button
    CV button is now centered using flex on mobile.
    Tweaked font home-card font sizes on mobile.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './header.css'
 import logo from '../assets/dev-logo.svg'
 
 function Header() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header>
       <div className="header-container-left">
@@ -14,19 +21,24 @@ function Header() {
         <h1>Anthony <div className="impact"> Yuen</div></h1>
       </div>
       <div className="header-container-right">
-        <nav>
+        <div className={`burger-menu ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+        <nav className={`nav-menu ${isMenuOpen ? 'open' : ''}`}>
           <ul>
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/" onClick={toggleMenu}>Home</Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link to="/about" onClick={toggleMenu}>About</Link>
             </li>
             <li>
-              <Link to="/projects">Projects</Link>
+              <Link to="/projects" onClick={toggleMenu}>Projects</Link>
             </li>
             <li>
-              <Link to="/contact">Contact</Link>
+              <Link to="/contact" onClick={toggleMenu}>Contact</Link>
             </li>
           </ul>
         </nav>
