98bd3951ba6bfb3e43dcc38ef1fc596ee33c86b6
An1honyY <ant.jo.yuen@gmail.com>
2024-03-07 18:13:41 +1300

    Menu close when clicking outside and animation removed for mobile

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../assets/dev-logo.svg'
 import './header.css'
@@ -7,24 +7,42 @@ import './header.css'
 function Header() {
   // Burger Menu Things
   const [isMenuOpen, setMenuOpen] = useState(false);
-
+  const menuRef = useRef(null);
   const toggleMenu = () => {
     setMenuOpen(!isMenuOpen);
   };
+
+  useEffect(() => {
+    
+    // Close the hamburger menu when clicking outside of it
+    const handleDocumentClick = (event) => {
+      if (isMenuOpen && menuRef.current && !menuRef.current.contains(event.target)) {
+        document.querySelector('.burger-menu').classList.remove('open');
+        document.querySelector('.nav-menu').classList.remove('open');
+        toggleMenu();
+      }
+    };
+
+    document.addEventListener('click', handleDocumentClick);
+
+    return () => {
+      document.removeEventListener('click', handleDocumentClick);
+    };
+  }, [menuRef, isMenuOpen]);
+
 
   return (
     <header>
       <div className="header-container-left">
         <div className="logo">
           <img src={logo} alt="logo" />
-
         </div>
         <Link to="/">
           <h1>Anthony <div className="impact"> Yuen</div></h1>
         </Link>
       </div>
       <div className="header-container-right">
-        <div className={`burger-menu ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        <div ref={menuRef} className={`burger-menu ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
           {/* Burger Menu Bars */}
           <div className={`bar ${isMenuOpen ? 'open' : ''}`}></div>
           <div className={`bar-middle ${isMenuOpen ? 'open' : ''}`}></div>
