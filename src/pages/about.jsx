206f6768ecc8c8c484631ccc2f61f79adee978ba
An1honyY <ant.jo.yuen@gmail.com>
2024-03-25 15:01:52 +1300

    Updated CV and About

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -14,9 +14,22 @@ function About() {
             </h1>
             <br></br>
             <section className="about-card">
-                <p>Currently practicing Web development skills. This website helps with that :)</p>
+                <h3>Who am I?</h3>
+                <p>I'm a fresh job seeker with an academic background in IT.</p>
+                <p>Roles I'd be best fit for are Helpdesk Support and Junior Developer.</p> 
+                <p>I'm also interested in obtaining job experience elsewhere.</p>
+                <p>Customer Service is a key skill I would like to learn.</p>
+                <br></br>
+                <h3>Background</h3>
+                <p>I have a solid tech foundation from my Bachelors of Computer Science and Information Science in Digital Services and Software Development.</p>
+                <p>I gained experience in game, IoT and web development, using languages such as Java, C#, Python, PHP and React.js. </p>
+                <p>I was also given the chance to learn concepts such as the ITIL framework for services, needs analysis and security management.</p>
+                <p>My studies involved various team-based projects to give me practical job skills - Communication and Teamwork being chief of the soft skills I obtained.</p>
+                <p>For job experience, I've done part time gardening and violin tutoring, which have built up my resilience and patience respectively.</p>
 
-                <p>I'm also learning Active Directory with the aim of getting an IT role.</p>
+                <br></br>
+                <p>Currently, I'm practicing Web development. This website helps with that :)</p>
+                <p>I'm also studying towards the A+ cert to provide me with more IT support related skills.</p>
             </section>
         </main>
     )
