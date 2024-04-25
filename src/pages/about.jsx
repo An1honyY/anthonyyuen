3a68bbeccb42d310b824b63ffbf215ae4f5938a0
An1honyY <ant.jo.yuen@gmail.com>
2024-04-25 17:09:10 +1200

    Update About

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -15,21 +15,25 @@ function About() {
             <br></br>
             <section className="about-card">
                 <h3>Who am I?</h3>
-                <p>I'm a fresh job seeker with an academic background in IT.</p>
+                <p>I'm a fresh job seeker with an academic background in ICT.</p>
                 <p>Roles I'd be best fit for are Helpdesk Support and Junior Developer.</p> 
-                <p>I'm also interested in obtaining job experience elsewhere.</p>
-                <p>Customer Service is a key skill I would like to learn.</p>
+                <br></br>
+                <p>I'm looking for a full-time entry-level job.</p> 
+                <p>I'm also interested in other part-time work in the meanwhile.</p>
                 <br></br>
                 <h3>Background</h3>
-                <p>I have a solid tech foundation from my Bachelors of Computer Science and Information Science in Digital Services and Software Development.</p>
-                <p>I gained experience in game, IoT and web development, using languages such as Java, C#, Python, PHP and React.js. </p>
+                <p>I attended Auckland University of Technology.</p>
+                <p>There, I gained a solid tech foundation from my Bachelors of Computer and Information Sciences in Digital Services and Software Development.</p>
+                <br></br>
+                <p>I gained experience in Game, IoT and Web development, using languages such as Java, C#, Python, PHP and React.js. </p>
                 <p>I was also given the chance to learn concepts such as the ITIL framework for services, needs analysis and security management.</p>
+                <br></br>
                 <p>My studies involved various team-based projects to give me practical job skills - Communication and Teamwork being chief of the soft skills I obtained.</p>
-                <p>For job experience, I've done part time gardening and violin tutoring, which have built up my resilience and patience respectively.</p>
+                <p>Job experience includes part time gardening and violin tutoring, which have built up my resilience and people patience.</p>
 
                 <br></br>
                 <p>Currently, I'm practicing Web development. This website helps with that :)</p>
-                <p>I'm also studying towards the COMPTIA A+ cert to provide me with more IT support related skills.</p>
+                <p>I'm also studying towards the COMPTIA A+ cert to provide me with stronger foundational IT support related skills.</p>
             </section>
         </main>
     )
