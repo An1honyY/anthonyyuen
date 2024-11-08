bc923369ff96f10bae39d21686d49b72d4aa4435
An1honyY <ant.jo.yuen@gmail.com>
2024-11-08 13:31:37 +1300

    Updated About
    Added new images including a new Zealand image based on
    M.Bitton, CC BY-SA 4.0 <https://creativecommons.org/licenses/by-sa/4.0>, via Wikimedia Commons
    
    Also added padding to about images and made changes to text.

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,11 +4,16 @@ import diploma_img from '../assets/diploma.svg'
 import grad_img from '../assets/grad.svg'
 import java_img from '../assets/java.svg'
 import python_img from '../assets/python.svg'
+import php_img from '../assets/php.svg'
 import react_img from '../assets/react.svg'
+import mysql_img from '../assets/mysql.svg'
+import mongodb_img from '../assets/mongodb.svg'
 import shears_img from '../assets/shears.svg'
 import violin_img from '../assets/violin.svg'
 import group_img from '../assets/group.svg'
 import webdev_img from '../assets/webdev.svg'
+import auckland_img from '../assets/auckland2.webp'
+import car_img from '../assets/car.svg'
 
 function About() {
     return (
@@ -23,7 +28,12 @@ function About() {
                 <p>Roles I'd be best fit for are Helpdesk Support and Software Developer.</p> 
                 <br></br>
                 <p>However, I'm interested in any entry-level job offers.</p> 
-                <p>I'm based in Auckland, with a Restricted Drivers License.</p>
+                <p>I'm based in West Auckland, with a Restricted Drivers License.</p>
+                <img src={auckland_img} style={{width: '8rem'}}></img>
+                <img src={car_img}></img>
+                <br></br>
+                <br></br>
+
                 <h3>Background</h3>
                 <br></br>
                 <p>I attended Auckland University of Technology.</p>
@@ -34,15 +44,18 @@ function About() {
                 <br></br>
                 <br></br>
                 <p>I gained experience in Game, IoT and Web development, using languages such as Java, C#, Python, PHP and React.js. </p>
-                <p>I was also given the chance to learn concepts such as the ITIL framework for services, needs analysis and security management.</p>
+                <p>I also learnt concepts such as the ITIL framework for services, needs analysis and security management.</p>
                 <br></br>
                 <img src={java_img}></img>
                 <img src={python_img}></img>
+                <img src={php_img}></img>
                 <img src={react_img}></img>
+                <img src={mysql_img}></img>
+                <img src={mongodb_img}></img>
                 <br></br>
                 <br></br>
-                <p>My studies involved various team-based projects to give me practical soft skills - Problem Solving, Communication and Teamwork.</p>
-                <p>Job experience includes part-time gardening and violin tutoring, which have built up my tenacity and people patience.</p>
+                <p>My studies involved practical team-based projects which developed my Problem Solving, Communication and Teamwork skills.</p>
+                <p>Past job experience includes part-time gardening and violin tutoring, which taught me how to work independently and creatively.</p>
                 <br></br>
                 <img src={group_img}></img>
                 <img src={shears_img}></img>
@@ -51,7 +64,7 @@ function About() {
                 <br></br>
                 <p>At the moment I'm practising Web development.</p>
                 <p>My current project is a Mini Essay web app to encourage better learning using the Feynman technique.</p>
-                <p>(That's where you learn through teaching. In this case, through a short typed format)</p>
+                <p>It uses React with Typescript, the DaisyUI component library, a WYSIWYG text editor called BlockNote, and MongoDB as the backend.</p>
                 <br></br>
                 <img src={webdev_img}></img>
             </section>
